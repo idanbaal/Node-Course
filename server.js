add8b5f5fe2799b16c3203ece268cce667e54ebf
idanbaal <idanbaal@post.bgu.ac.il>
2018-08-29 18:06:22 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine','hbs');
+
+app.use((req, res, next) =>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log' , log + '\n' , (error) => {
+    if (error)
+      console.log('unable to append to server.log');
+   });
+   next();
+});
+
+app.use((req, res, next) =>{
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+
+app.get('/',(req,res) =>{
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    wellcomeMessage: 'Wellcome To My website'
+  })
+});
+
+
+app.get('/about',(req,res) =>{
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad',(req,res) =>{
+  res.send({
+    errorMessage : 'error handle'
+  });
+});
+
+
+
+app.listen(port, () => {
+console.log(`Server is up in port ${port}`);});
